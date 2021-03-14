9b8bacd0e7807a63764f2f2e5f88189c433aea4e
문요한 <munyohan@naver.com>
2021-03-14 23:59:49 +0900

    Update today's algorithm 오늘은 프로그래머스의 스택/큐 '다리를 지나는 트럭' 문제를 풀었습니다. 어제부터 풀어가지고 좀 오래걸렸지만 배열을 forEach로 돌때, shift()연산을 하면 인덱스가 넘어가진다는 점을 간과하여 오래걸렸습니다. 앞으로는 이런 실수를 줄여야 겠습니다.

---
+++ 2021년/3월/8일 ~ 14일/다리를 지나는 트럭.js
@@ -0,0 +1,56 @@
+"use strict";
+
+function solution(bridge_length, weight, truck_weights) {
+      class Truck {
+    constructor(weight) {
+      this.weight = weight;
+      this.timer = 1;
+    }
+
+    move() {
+      this.timer++;
+
+      if (this.timer > bridge_length) return true;
+      else return false;
+    }
+
+    toString() {
+      return `weight: ${this.weight}, timer: ${this.timer}`;
+    }
+  }
+
+  let totalSec = 0;
+  const trucksOnBridge = [];
+
+  while (true) {
+    let weightOfTrucksOnBridge = 0;
+
+    // 다리를 건너는 트럭 한 칸씩 전진
+    for(let i =0; i< trucksOnBridge.length; i++) {
+      if (trucksOnBridge[i].move()) {
+        trucksOnBridge.shift();
+        i--;
+      }
+    }
+
+    // 다리에 새로운 트럭이 진입할 수 있으면 진입
+    trucksOnBridge.forEach((truck) => {
+      weightOfTrucksOnBridge += truck.weight;
+    });
+
+    if (weightOfTrucksOnBridge + truck_weights[0] <= weight) {
+      trucksOnBridge.push(new Truck(truck_weights.shift()));
+    }
+
+
+    // 여기까지 1초
+    totalSec++;
+
+    // 다리위에랑 기다리는 트럭이 둘 다 없으면 break
+    if (trucksOnBridge.length === 0 && truck_weights.length === 0) break;
+  }
+
+  return totalSec;
+}
+
+console.log(solution(2, 10, [7, 4, 5, 6]));
