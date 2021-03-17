de52cf4fdd6234484a12a1082aa8ff51e551c5d9
문요한 <munyohan@naver.com>
2021-03-17 23:30:57 +0900

    최대힙도 구현 하지만 따로 구현하면 각각 다른 배열을 갖고 있기 때문에 데이터의 일치가 안됨. 아 그리고 굳이 0으로 초기화 신경 안써줘도 되는게 결국 완전이진트리이고 lastIndex라는 기준이 있기 때문에 push할 때 값만 잘 들어가면 상관없음

--- 2021년/3월/15일 ~ 21일/이중우선순위큐.js
+++ 2021년/3월/15일 ~ 21일/이중우선순위큐.js
@@ -8,11 +8,11 @@ function solution(operations) {
     }
 
     pop() {
-      if (this.lastIndex === 0) throw Error("Heap is empty !");
+      if (this.lastIndex === 0) return null;
 
       const minValue = this.arr[1];
       this.arr[1] = this.arr[this.lastIndex];
-      this.arr[this.lastIndex--] = 0;
+      this.arr[this.lastIndex--] = null;
 
       let curIndex = 1;
       while (curIndex <= this.lastIndex) {
@@ -54,7 +54,76 @@ function solution(operations) {
     }
 
     peek() {
-      return this.arr[1];
+      if (this.lastIndex === 0) return 0;
+      else return this.arr[1] - 0;
+    }
+
+    print() {
+      let result = "";
+
+      this.arr.forEach((num) => {
+        result += num + " ";
+      });
+
+      console.log(result);
+    }
+  }
+
+  class MaxHeap {
+    constructor() {
+      this.arr = [];
+      this.lastIndex = 0;
+    }
+
+    pop() {
+      if (this.lastIndex === 0) return null;
+
+      const maxValue = this.arr[1];
+      this.arr[1] = this.arr[this.lastIndex];
+      this.arr[this.lastIndex--] = null;
+
+      let curIndex = 1;
+      while (curIndex <= this.lastIndex) {
+        let leftChildIndex = curIndex * 2;
+        let rightChildIndex = curIndex * 2 + 1;
+
+        let targetIndex = 0;
+        if (this.arr[leftChildIndex] > this.arr[rightChildIndex])
+          targetIndex = leftChildIndex;
+        else targetIndex = rightChildIndex;
+
+        if (this.arr[curIndex] >= this.arr[targetIndex]) break;
+
+        let temp = this.arr[curIndex];
+        this.arr[curIndex] = this.arr[targetIndex];
+        this.arr[targetIndex] = temp;
+
+        curIndex = targetIndex;
+      }
+
+      return maxValue;
+    }
+
+    push(num) {
+      this.arr[++this.lastIndex] = num;
+
+      let curIndex = this.lastIndex;
+      while (curIndex >= 1) {
+        let parentIndex = curIndex >> 1;
+
+        if (this.arr[curIndex] > this.arr[parentIndex]) {
+          let temp = this.arr[curIndex];
+          this.arr[curIndex] = this.arr[parentIndex];
+          this.arr[parentIndex] = temp;
+        } else break;
+
+        curIndex = parentIndex;
+      }
+    }
+
+    peek() {
+      if (this.lastIndex === 0) return 0;
+      else return this.arr[1] - 0;
     }
 
     print() {
@@ -70,6 +139,7 @@ function solution(operations) {
 
   let answer = [];
   const minHeap = new MinHeap();
+  const maxHeap = new MaxHeap();
 
   for (const operation of operations) {
     const [command, data] = operation.split(" ");
@@ -77,13 +147,14 @@ function solution(operations) {
     switch (command) {
       case "I":
         minHeap.push(data);
+        maxHeap.push(data);
         break;
 
       case "D":
         if (data === "-1") {
           minHeap.pop();
         } else if (data === "1") {
-          let trash = 0;
+          maxHeap.pop();
         } else {
           throw Error("Wrong operation !");
         }
@@ -96,7 +167,9 @@ function solution(operations) {
 
   minHeap.print();
 
+  answer = [maxHeap.peek(), minHeap.peek()];
+
   return answer;
 }
 
-solution(["I 16", "I 4", "I -5", "D -1"]);
+console.log(solution(["I 16", "D 1"]));
