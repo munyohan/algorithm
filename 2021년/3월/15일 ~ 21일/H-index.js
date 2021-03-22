87810a56cc0deb3c4209240ca5bb84cea0b55ab6
moon <munyohan@naver.com>
2021-03-22 09:41:46 +0900

    재 업로드

--- 2021년/3월/15일 ~ 21일/H-index.js
+++ 2021년/3월/15일 ~ 21일/H-index.js
@@ -1,13 +1,22 @@
-'use strict'
+"use strict";
 
 function solution(citations) {
-    let answer = 0;
+  let h = 0;
+  citations.sort((a, b) => a - b);
 
-    citations.sort((a, b) => a-b);
-    console.log(citations);
+  for (let i = 0; i < citations.length; i++) {
+    const item = citations[i];
+    const rest = citations.length - i;
 
-    return answer;
+    // 논문의 인용횟수가 현재 값보다 크면 현재 값을 증가시켜서 다시 검사
+    // 만약 h가 h이상인 논문의 수보다 많아지면 그만 검사
+    if (item > h && h < rest) {
+      h++;
+      i--;
+    }
+  }
+
+  return h;
 }
 
 console.log(solution([3, 0, 6, 1, 5]));
-
