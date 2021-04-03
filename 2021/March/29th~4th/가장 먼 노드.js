8e7388b35beb141f7940d4f31cca43ccad70d53f
문요한 <munyohan@naver.com>
2021-04-03 23:59:54 +0900

    프로그래머스 가장 먼 노드 풀이
    근데 Dfs로 풀어서 안됨. 심지어 로직도 틀림...
    bfs로 다시도전하겠음.

--- 2021/March/29th~4th/가장 먼 노드.js
+++ 2021/March/29th~4th/가장 먼 노드.js
@@ -8,8 +8,58 @@
 */
 function solution(n, edge) {
   let answer = 0;
+  const minDistances = new Array(n + 1).fill(20000);
 
-  // 먼저 인접행렬을 만들어준다.
+  for (let i = 1; i <= n; i++) {
+    calculate(1, i, [1], 0);
+  }
+
+  console.log(minDistances);
+
+  let max = 0;
+  for (let i = 1; i < minDistances.length; i++) {
+    if (max < minDistances[i]) {
+      max = minDistances[i];
+      answer = 1;
+    } else {
+      answer++;
+    }
+  }
 
   return answer;
+
+  function calculate(start, dest, visited, distance) {
+    if (minDistances[dest] < distance) return;
+
+    if (start === dest && minDistances[dest] > distance) {
+      minDistances[dest] = distance;
+
+      return;
+    }
+
+    for (let i = 0; i < edge.length; i++) {
+      if (edge[i][0] === start && !visited.includes(edge[i][1])) {
+        calculate(edge[i][1], dest, [...visited, edge[i][1]], distance + 1);
+      } else if (edge[i][1] === start && !visited.includes(edge[i][0])) {
+        calculate(edge[i][0], dest, [...visited, edge[i][0]], distance + 1);
+      }
+    }
+  }
 }
+
+console.log(
+  solution(11, [
+    [1, 2],
+    [1, 3],
+    [2, 4],
+    [2, 5],
+    [3, 5],
+    [3, 6],
+    [4, 8],
+    [4, 9],
+    [5, 9],
+    [5, 10],
+    [6, 10],
+    [6, 11],
+  ])
+);
