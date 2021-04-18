9a417b05778ab75c7caf853c16ad190671a302ad
yohan Moon <munyohan@naver.com>
2021-04-18 15:45:58 +0900

    64. Minimun Path Sum 문제 좀 더 깔끔하게 수정
    근데 읽기에는 전 코드가 더 좋은 것 같다.

--- 2021/April/12th~18th/64. Minimum Path Sum.js
+++ 2021/April/12th~18th/64. Minimum Path Sum.js
@@ -1,20 +1,13 @@
 const minPathSum = grid => {
   for (let i = 0; i < grid.length; i++) {
     for (let j = 0; j < grid[0].length; j++) {
-      const cur = grid[i][j];
-      let up = 0;
-      let left = 0;
-
-      if (i - 1 >= 0) up = grid[i - 1][j];
-      if (j - 1 >= 0) left = grid[i][j - 1];
-
-      const upSum = cur + up;
-      const leftSum = cur + left;
-
       if (i - 1 < 0 && j - 1 < 0) continue;
-      else if (i - 1 < 0) grid[i][j] = leftSum;
-      else if (j - 1 < 0) grid[i][j] = upSum;
+      else if (i - 1 < 0) grid[i][j] += grid[i][j - 1];
+      else if (j - 1 < 0) grid[i][j] += grid[i - 1][j];
       else {
+        const upSum = grid[i][j] + grid[i - 1][j];
+        const leftSum = grid[i][j] + grid[i][j - 1];
+
         if (upSum < leftSum) grid[i][j] = upSum;
         else grid[i][j] = leftSum;
       }
