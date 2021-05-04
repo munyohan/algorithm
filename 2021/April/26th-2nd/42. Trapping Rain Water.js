59d5204efd65e1dbc9492472473e181bf8b6f76d
yohan Moon <munyohan@naver.com>
2021-05-04 09:10:25 +0900

    Upload

---
+++ 2021/April/26th-2nd/42. Trapping Rain Water.js
@@ -0,0 +1,7 @@
+const trap = height => {
+  let answer = 0;
+
+  return answer;
+};
+
+console.log(trap([4, 2, 0, 3, 2, 5])); // expected: 9
---
+++ 2021/May/3rd~9th/test.js
@@ -0,0 +1,19 @@
+const mergeKLists = lists => {
+  const answer = [];
+
+  return answer;
+
+  function insert(number) {
+    answer.push(number);
+
+    const index = answer.length;
+  }
+};
+
+console.log(
+  mergeKLists([
+    [1, 4, 5],
+    [1, 3, 4],
+    [2, 6],
+  ])
+);
